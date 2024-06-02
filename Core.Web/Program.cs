471962744a9012a3b327e4680917fad76013dc8f
Nichlas Hoberg <nichlashoberg@gmail.com>
2024-06-02 14:24:08 +0200

    feat: initial blaszor application

---
+++ Core.Web/Program.cs
@@ -0,0 +1,45 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+using Core.Web.Client.Pages;
+using Core.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBlazorise(opt =>
+    {
+        opt.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
---
+++ Dockerfile
@@ -0,0 +1,23 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG version
+ARG NUGET_USERNAME
+ARG NUGET_TOKEN
+WORKDIR /src
+
+COPY ["Core.Web/Core.Web.csproj", "Core.Web/"]
+COPY ["NuGet.config", "Core.Web/"]
+
+RUN dotnet restore "Core.Web/Core.Web.csproj" --configfile Core.Web/NuGet.config
+
+COPY . .
+
+RUN dotnet publish "Core.Web/Core.Web.csproj" -c Release -o out /p:Version=$version
+
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 
+WORKDIR /app
+
+EXPOSE 80
+EXPOSE 443
+
+COPY --from=build /src/out .
+ENTRYPOINT ["dotnet", "Core.Web.dll"]
